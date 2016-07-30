c345bbf1b9f3254ed6178ddcd281bec6099f3206
Clancy Kornie <clancy.kornie@imcpacific.com.au>
2016-07-30 13:38:46 +1000

    Hello Clancy

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello Vance!");
+		System.out.println("Hello Clancy!");
 		System.out.println("Goodbye World!");
 	}
 }
