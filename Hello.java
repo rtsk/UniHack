96c989b545fbd20725b8d7443ef4526944cabbe2
Ben Vance <ben.vance@imcpacific.com.au>
2016-07-30 13:37:57 +1000

    Hello only to Vance

--- Hello.java
+++ Hello.java
@@ -2,7 +2,7 @@ class Hello
 {
 	public static void main(String[] args)
 	{
-		System.out.println("Hello World!");
+		System.out.println("Hello Vance!");
 		System.out.println("Goodbye World!");
 	}
 }
